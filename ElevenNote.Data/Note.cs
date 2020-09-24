29db5b760a14d3df21f25c67acc965a88800a7ff
bmagee2 <bamagee2@gmail.com>
2020-09-24 11:45:50 -0400

    added Note

---
+++ ElevenNote.Data/Note.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+    // The entity model represents how we will set up data in our database. The properties we add to this class will be added as columns in our SQL table. This class will also be used by the service layer to persist data and query the database.s
+    public class Note
+    {
+        [Key]   // attribute specifies the property that uniquely identifies an entity; the primary key will always be a unique number.
+        public int NoteId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Your Note")]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
